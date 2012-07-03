78a052f05bf06ad38a057bdd109ee03000c458fe
alexcr-telecom <alexcr.telecom@gmail.com>
2012-07-03 14:53:07 +0400

    перевод 3

--- lang/admin/ru_ru/time_manager.js
+++ lang/admin/ru_ru/time_manager.js
@@ -1,6 +1,7 @@
-Object.extend(switchvox.lang._LANG.keys,{PT_TIME_FRAMES:["Time Frames"],
-SH_CREATE_TF:["Create Time Frame"],
-L_TF_NAME:["Time Frame Name"],
-TBL_NO_TF_DISPLAY:["There are no time frames to display."],
-T_CREATED_BY_ADMIN:["Admin"],
-T_CREATED_BY_USER:["User"]});
+Object.extend(switchvox.lang._LANG.keys,{
+PT_TIME_FRAMES:["Временные рамки"],
+SH_CREATE_TF:["Создать временные рамки"],
+L_TF_NAME:["Имя временных рамок"],
+TBL_NO_TF_DISPLAY:["Нет временных рамок для отображения."],
+T_CREATED_BY_ADMIN:["Администратор"],
+T_CREATED_BY_USER:["Пользователь"]});
